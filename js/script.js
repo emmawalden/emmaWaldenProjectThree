a3534e01cd563cf3c8e334e9435213eb21205ed8
Emma Walden <emmajanewalden@gmail.com>
2020-03-31 22:41:50 -0400

    changed background image, fonts, added linear gradient

--- js/script.js
+++ js/script.js
@@ -45,50 +45,56 @@ $(document).ready(function() {
         const answer2 = $("input[name=water]:checked").val(); 
         const answer3 = $("input[name=record]:checked").val();
 
+        // Sweet alert, may not need if radio buttons are always checked
+        // if (answer1.checked === false | answer2.checked === false | answer3.checked === false) {
+        //     swal({
+        //         text: "Please pick a response",
+        //         icon: "error",
+        //     });
+        // }
+        
+
     // convert answers into numbers to be able to add them 
         const sum = parseInt(answer1) + parseInt(answer2) + parseInt(answer3);
-        console.log(sum);
+
+
+    // Creating variables to be added to the DOM
+        const yourResult = document.createElement('div');
+        yourResult.classList.add("resultStyles");
+        const yourResultImage = document.createElement('img');
+        yourResultImage.classList.add("imageStyles")
+        const yourResultHeading = document.createElement('h3');
     
     // Conditions to match the sum of the user responses with a plant
         if (sum >= 30) { 
-            const yourResult = document.createElement('div');
-            yourResult.classList.add("resultStyles");
-            const yourResultImage = document.createElement('img');
             yourResultImage.src = "./assets/ficus.jpeg";
-            yourResultImage.classList.add("imageStyles")
-            yourResult.appendChild(yourResultImage)
+            yourResultHeading.textContent = "You're a pro get yourself a Fiddle Leaf Fig!";
+            yourResult.appendChild(yourResultHeading);
+            yourResult.appendChild(yourResultImage);
             document.body.appendChild(yourResult);
         } else if (sum >= 25) {
-            const yourResult = document.createElement('div');
-            yourResult.classList.add("resultStyles");
-            const yourResultImage = document.createElement('img');
             yourResultImage.src = "./assets/zz.jpeg";
-            yourResultImage.classList.add("imageStyles")
-            yourResult.appendChild(yourResultImage)
+            yourResultHeading.textContent = "Try out a Zanzibar Gem!";
+            yourResult.appendChild(yourResultHeading);
+            yourResult.appendChild(yourResultImage);
             document.body.appendChild(yourResult);
         } else if (sum === 20 ) {
-            const yourResult = document.createElement('div');
-            yourResult.classList.add("resultStyles");
-            const yourResultImage = document.createElement('img');
             yourResultImage.src = "./assets/succulent.jpeg";
-            yourResultImage.classList.add("imageStyles")
-            yourResult.appendChild(yourResultImage)
+            yourResultHeading.textContent = "Try a trusty succulent";
+            yourResult.appendChild(yourResultHeading);
+            yourResult.appendChild(yourResultImage);
             document.body.appendChild(yourResult);
         } else if (sum === 15 || sum === 10) {
-            const yourResult = document.createElement('div');
-            yourResult.classList.add("resultStyles");
-            const yourResultImage = document.createElement('img');
             yourResultImage.src = "./assets/snakePlant.jpeg";
-            yourResultImage.classList.add("imageStyles")
-            yourResult.appendChild(yourResultImage)
+            yourResultHeading.textContent = "Try out a snake plant";
+            yourResult.appendChild(yourResultHeading);
+            yourResult.appendChild(yourResultImage);
             document.body.appendChild(yourResult);
         } else if(sum <= 5) {
-            const yourResult = document.createElement('div');
-            yourResult.classList.add("resultStyles");
-            const yourResultImage = document.createElement('img');
             yourResultImage.src = "./assets/plastic.jpeg";
-            yourResultImage.classList.add("imageStyles")
-            yourResult.appendChild(yourResultImage)
+            yourResultHeading.textContent = "Best to stick to plastic!";
+            yourResult.appendChild(yourResultHeading);
+            yourResult.appendChild(yourResultImage);
             document.body.appendChild(yourResult);
         }
     }
