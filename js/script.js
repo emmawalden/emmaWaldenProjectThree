0aeca5983c01a278124679e604635d97d60f8df2
Emma Walden <emmajanewalden@gmail.com>
2020-04-06 17:12:49 -0400

    added CSS shine, made plant image responsive

--- js/script.js
+++ js/script.js
@@ -1,46 +1,54 @@
 const plants = {
-    ficus: [
+    ficus:
         {
             light: "So much",
             water: "Absolutely",
             record: "Everything I touch turns to green", 
+            text: "You're a pro get yourself a Fiddle Leaf Fig!",
             src: "./assets/ficus.jpeg",
-            text: "You're a pro get yourself a Fiddle Leaf Fig!" 
+            alt: "Fiddle Leaf Fig plant in basket"
         },
-    ],
-    zanzibar: [
+    zanzibar:
         {
             light: "Some",
             water: "Sometimes",
-            record: "I've had luck with succulents"
+            record: "I've had luck with succulents",
+            text: "Try out a Zanzibar Gem!",
+            src: "./assets/zz.jpg",
+            alt: "Sprig of a Zanzibar Gem"
         },
-    ],
-    succulent: [
+    succulent: 
         {
             light: "Some",
             water: "Sometimes",
-            record: "My home is a plant cemetary"
+            record: "My home is a plant cemetary",
+            text: "Try a trusty succulent",
+            src: "./assets/succulent.jpeg",
+            alt: "Succulent in a small vase"
         },
-    ],
-    snake: [
+    snake: 
         {
             light: "Some",
             water: "No",
-            record: "I've had luck with succulents"
+            record: "I've had luck with succulents",
+            text: "Try out a snake plant",
+            src: "./assets/snakePlant.jpeg",
+            alt: "Small snake plant in a terracotta pot"
         },
-    ],
-    plastic: [
+    plastic:
         {
             light: "None",
             water: "No",
-            record: "My home is a plant cemetary"
+            record: "My home is a plant cemetary",
+            text: "Best to stick to plastic",
+            src: "./assets/plastic.jpeg",
+            alt: "Close up of a plastic plant"
         },
-    ]
 };
 
 $(document).ready(function() {
     
-    $("form").on("submit", function (event) {
+    $("form").on("submit", function(event) {
         //  prevent default action, 
         event.preventDefault();
 
@@ -53,30 +61,36 @@ $(document).ready(function() {
     // convert answers into numbers to be able to add them 
         const sum = parseInt(answer1) + parseInt(answer2) + parseInt(answer3);
 
+        
     // Conditions to match the sum of the user responses with a plant OR alert user if they don't answer all the questions
         if (sum >= 30) { 
-            $(".quizResults").html(`<h3>You're a pro get yourself a Fiddle Leaf Fig!</h3>
-            <img class="imageStyles" src="./assets/ficus.jpeg" alt"Fiddle Leaf Fig plant in basket">`);
+            $(".quizResults").html(`<h3>${plants.ficus.text}</h3>
+            <img class="imageStyles" src="${plants.ficus.src}" alt="${plants.ficus.alt}">`);
+            smoothScroll('img');
         } else if (sum >= 25) {
-            $(".quizResults").html(`<h3>Try out a Zanzibar Gem!</h3>
-            <img class="imageStyles" src="./assets/zz.jpg" alt"Sprig of a Zanzibar Gem">`);
+            $(".quizResults").html(`<h3>${plants.zanzibar.text}</h3>
+            <img class="imageStyles" src="${plants.zanzibar.src}" alt="${plants.zanzibar.alt}">`);
         } else if (sum === 20 ) {
-            $(".quizResults").html(`<h3>Try a trusty succulent</h3>
-            <img class="imageStyles" src="./assets/succulent.jpeg" alt"Succulent in a small vase">`);
+            $(".quizResults").html(`<h3>${plants.succulent.text}</h3>
+            <img class="imageStyles" src="${plants.succulent.src}" alt="${plants.succulent.alt}">`);
         } else if (sum === 15 || sum === 10) {
-            $(".quizResults").html(`<h3>Try out a snake plant</h3>
-            <img class="imageStyles" src="./assets/snakePlant.jpeg" alt"Small snake plant in a terracotta pot">`);
+            $(".quizResults").html(`<h3>${plants.snake.text}</h3>
+            <img class="imageStyles" src="${plants.snake.src}" alt="${plants.snake.alt}">`);
         } else if(sum <= 5) {
-            $(".quizResults").html(`<h3>Best to stick to plastic</h3>
-            <img class="imageStyles" src="./assets/plastic.jpeg" alt"Close up of a plastic plant">`);
+            $(".quizResults").html(`<h3>${plants.plastic.text}</h3>
+            <img class="imageStyles" src="${plants.plastic.src}" alt="${plants.plastic.alt}">`);
         } else {
             swal({
             text: "Make sure to answer them all!",
             icon: "error",
                 });
-
-            };
+            };       
         });
+
+        $("button").click(function () {
+            alert($(".quizResults").scrollTop());
+        });
+
 
     // This button reloads the page and scrolls to the top to take quiz again
         $(".takeQuizAgain").on('click', function () {
