7cbc5cd0b4d7b802726cfa70e87863d7e4385e3b
Emma Walden <emmajanewalden@gmail.com>
2020-04-09 16:27:18 -0400

    added image credits and more plant images

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const plantValue = {
     30: "ficus",
-    25: "zanzibar",
+    25: "monstera",
     20: "succulent",
-    15: "snake",
+    15: "zanzibar",
     10: "snake",
     5: "plastic",
     0: "plastic",
@@ -11,30 +11,42 @@ const plantValue = {
 const plants = [
         {
             text: "You're a pro get yourself a Fiddle Leaf Fig!",
+            // Photo by Lauren Mancke on Unsplash
             src: "./assets/ficus.jpeg",
             alt: "Fiddle Leaf Fig plant in a basket",
             tag: "ficus",
         },
         {
-            text: "Try out a Zanzibar Gem!",
-            src: "./assets/zz.jpg",
-            alt: "Sprig of a Zanzibar Gem",
-            tag: "zanzibar"
+            text: "Get yourself a Monstera",
+            // Photo by Kara Eads on Unsplash
+            src: "./assets/monstera.jpeg",
+            alt: "Monstera in a pot on a small wooden table",
+            tag: "monstera",
         },
         {
             text: "Try a trusty succulent",
+            // Photo by Jeremy Lwanga on Unsplash
             src: "./assets/succulent.jpeg",
             alt: "Succulent in a small vase",
             tag: "succulent",
         }, 
         {
+            text: "Try out a Zanzibar Gem!",
+            // Photo by Paweł Czerwiński on unsplash
+            src: "./assets/zz.jpg",
+            alt: "Sprig of a Zanzibar Gem",
+            tag: "zanzibar"
+        },
+        {
             text: "Try out a snake plant",
+            // Photo by Kara Eads on Unsplash
             src: "./assets/snakePlant.jpeg",
             alt: "Small snake plant in a terracotta pot",
             tag: "snake",
         },
         {
             text: "Best to stick to plastic",
+            // Photo by Kristine Tanne on unsplash
             src: "./assets/plastic.jpeg",
             alt: "Close up of a plastic plant",
             tag: "plastic",
@@ -80,13 +92,13 @@ $(document).ready(function() {
                 plantAlt = plants[i].alt;
             };
         };  
+    
 
     //  Dynamically add html of heading and image to the page
         $(".quizResults").html(`<h3>${plantText}</h3>
         <img class="imageStyles" src="${plantImage}" alt="${plantAlt}"></img>
             `);
         });
-
 
     // This button reloads the page and scrolls to the top to take quiz again
         $(".takeQuizAgain").on('click', function () {
